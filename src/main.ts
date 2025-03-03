5345795e44a32db8c144f6fc27b5c76ef4904ce4
gufir <gunawan.firdaus99@ymail.com>
2025-03-03 15:29:30 +0700

    feat(vercel): add vercel.json

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@mdi/font/css/materialdesignicons.min.css'
+import { createHead } from '@vueuse/head'
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(router)
+app.use(head)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { nextTick } from 'vue'
+import { useHead } from '@vueuse/head'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: () => import('@/views/HomeView.vue') },
+    {
+      path: '/',
+      component: () => import('@/views/HomeView.vue'),
+      meta: {
+        title: 'Gunawan Firdaus Website',
+        description: 'Welcome To Gunawan Firdaus Website!',
+      },
+    },
     // { path: '/about', component: () => import('@/views/AboutView.vue') },
     // { path: '/tech', component: () => import('@/views/TechView.vue') },
     // { path: '/project', component: () => import('@/views/ProjectView.vue') },
@@ -12,6 +20,10 @@ const router = createRouter({
       path: '/:pathMatch(.*)*',
       name: 'NotFound',
       component: () => import('@/views/NotFound.vue'),
+      meta: {
+        title: 'Halaman Tidak Ditemukan',
+        description: 'Oops! Halaman yang Anda cari tidak tersedia.',
+      },
     },
   ],
 
@@ -29,6 +41,20 @@ const router = createRouter({
       })
     })
   },
+})
+
+router.beforeEach((to) => {
+  nextTick(() => {
+    useHead({
+      title: (to.meta.title as string) || 'Gunawan Firdaus Website',
+      meta: [
+        {
+          name: 'description',
+          content: (to.meta.description as string) || 'Welcome To Gunawan Firdaus Website!',
+        },
+      ],
+    })
+  })
 })
 
 export default router
